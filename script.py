b4dbbaaa3f98224b16fc5b64c14c0ca33977f667
nvimueller <ismoliveira@inf.ufrgs.br>
2026-08-22 13:59:26 -0300

    td x th analysis

--- script.py
+++ script.py
@@ -1,29 +1,46 @@
 from bokeh.plotting import figure, save, output_file
 from bokeh.models import CustomJS
 
-with open("data.txt", "r") as f:
-    data = [line.split() for line in f if line.strip()]
+with open("32nm_lp.txt", "r") as file:
+    data_32nm_lp = [line.split() for line in file if line.strip()]
+with open("45nm_lp.txt", "r") as file:
+    data_45nm_lp = [line.split() for line in file if line.strip()]
+with open("32nm_hp.txt", "r") as file:
+    data_32nm_hp = [line.split() for line in file if line.strip()]
+with open("45nm_hp.txt", "r") as file:
+    data_45nm_hp = [line.split() for line in file if line.strip()]
 
-time_us = [float(row[0]) * 1e9 for row in data]
-v_in    = [float(row[1]) for row in data]
-v_out   = [float(row[2]) for row in data]
-# i_vdd   = [float(row[3]) for row in data]
+
+th_32nm_lp = [float(row[1]) for row in data_32nm_lp]
+td_32nm_lp = [float(row[0]) for row in data_32nm_lp]
+th_45nm_lp = [float(row[1]) for row in data_45nm_lp]
+td_45nm_lp = [float(row[0]) for row in data_45nm_lp]
+th_32nm_hp = [float(row[1]) for row in data_32nm_hp]
+td_32nm_hp = [float(row[0]) for row in data_32nm_hp]
+th_45nm_hp = [float(row[1]) for row in data_45nm_hp]
+td_45nm_hp = [float(row[0]) for row in data_45nm_hp]
+
 
 output_file("plot.html", title="ngspice")
 
-p = figure(
+plot = figure(
         title="ngspice",
-        x_axis_label="time (ns)",
-        y_axis_label="voltage (v)",
+        x_axis_label="threshold voltage (V)",
+        y_axis_label="propagation delay (ns)",
         sizing_mode="stretch_both",
         tools="pan,wheel_zoom,box_zoom,reset,hover,save"
         )
 
-p.line(time_us, v_in, legend_label="v(in)", line_width=2, color="#1f77b4")
-p.line(time_us, v_out, legend_label="v(out)", line_width=2, color="#ff7f0e")
-p.legend.click_policy = "hide"
+colors = ["#0072B2", "#56B4E9", "#E69F00", "#D55E00"]
 
-save(p)
+plot.line(th_32nm_lp, td_32nm_lp, legend_label="32nm_lp", line_width=2, color=colors[0])
+plot.line(th_45nm_lp, td_45nm_lp, legend_label="45nm_lp", line_width=2, color=colors[2])
+plot.line(th_32nm_hp, td_32nm_hp, legend_label="32nm_hp", line_width=2, color=colors[1])
+plot.line(th_45nm_hp, td_45nm_hp, legend_label="45nm_hp", line_width=2, color=colors[3])
+
+plot.legend.click_policy = "hide"
+
+save(plot)
 
 touch_fix_css = """
 <style>
@@ -38,10 +55,10 @@ touch_fix_css = """
 </style>
 """
 
-with open("plot.html", "r") as f:
-    html_content = f.read()
+with open("plot.html", "r") as file:
+    html_content = file.read()
 
 html_content = html_content.replace("</head>", f"{touch_fix_css}\n</head>")
 
-with open("plot.html", "w") as f:
-    f.write(html_content)
+with open("plot.html", "w") as file:
+    file.write(html_content)
